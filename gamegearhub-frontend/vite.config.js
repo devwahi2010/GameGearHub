768ad2e209a9f4e94a4284fd0faadbf64579155b
Dev Wahi <devwahi@Devs-MacBook-Air.local>
2025-06-21 23:28:28 +0530

    Include dist folder for manual deployment

--- gamegearhub-frontend/vite.config.js
+++ gamegearhub-frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',  // Required for Render deployment and BrowserRouter routing
   plugins: [react()],
   build: {
     outDir: 'dist',
-  }
-})
-  
+  },
+});
